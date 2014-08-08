7761ff4bce9317bbcc487d57ece3e1dacafca6b1
Ryan Dumlao <dlogsr@gmail.com>
2014-08-07 23:46:44 -0700

    Finish sign in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+	def new
+	end
+
+	def create
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			#sign in the user
+			sign_in user
+			redirect_to user
+		else
+			#create an error message and re-render
+			flash.now[:error] = 'Invalid email/password combo'
+			render 'new'
+		end
+	end
+
+	def destroy
+		sign_out
+		redirect_to root_url
+	end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,15 +10,17 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-  		flash[:success] = "Welcome to the Sample App, FOO"
+      sign_in @user
+  		flash[:success] = "Welcome to the Sample App, "+@user[:name]+"!"
   		redirect_to @user
   	else
   		render 'new'
   	end
   end
 
+
   private
   	def  user_params
   		params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  	end  	
+  	end
 end
