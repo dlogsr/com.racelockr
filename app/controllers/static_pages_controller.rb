764e9438ed64014c7a63aee17eae86284df880f8
Ryan Dumlao <dlogsr@gmail.com>
2014-08-27 00:42:53 -0700

    removed 'race_feed' labeling to stop ambiguity (replaced with race_list).

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,6 @@ class StaticPagesController < ApplicationController
   	if signed_in?
   		@micropost = current_user.microposts.build
   		@feed_items = current_user.feed.paginate(page: params[:page])
-      @race_feed = current_user.races.paginate(page: params[:page])
   	end
   end
 
