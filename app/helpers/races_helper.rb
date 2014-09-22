ddf762862c1b45e8e93e69b142a04960d618a6a3
Ryan Dumlao <dlogsr@gmail.com>
2014-09-22 13:12:20 -0700

    added pace to races, need to fix less-then-10-second rounding

--- app/helpers/races_helper.rb
+++ app/helpers/races_helper.rb
@@ -11,4 +11,13 @@ module RacesHelper
 			"Triathlon"
 		end
 	end
+
+	#can you pass full model instances to a helper class?
+	def racepace(time,distance)
+		pace_total = (time[:hours].to_i * 60 + time[:minutes].to_i + time[:seconds].to_f / 60) / distance
+		# pace = (time[:hours].to_i * 60 + time[:minutes] + time[:seconds] / 60) / distance
+		pace_seconds = ( pace_total % 1 * 60 ).truncate
+		pace = pace_total.truncate.to_s + ":" + pace_seconds.to_s
+		pace
+	end
 end
